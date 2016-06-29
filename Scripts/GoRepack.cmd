8e4d2e9c8bf293d9b78cb4e2dfab72493384f627
Devedse <davydavidse@gmail.com>
2016-06-30 01:33:32 +0200

    Added test echo

--- Scripts/GoRepack.cmd
+++ Scripts/GoRepack.cmd
@@ -1,1 +1,2 @@
+echo test :)
 ..\packages\ILRepack.2.0.10\tools\ILRepack.exe /out:.\Output\DeveImagePyramid.exe ..\Devedse.DeveImagePyramid\bin\Release\Devedse.DeveImagePyramid.exe ..\Devedse.DeveImagePyramid\bin\Release\BitMiracle.LibTiff.NET.dll
