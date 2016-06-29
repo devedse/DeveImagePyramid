b18fc598326202210ef3240cd77f2e288537d845
Devedse <davydavidse@gmail.com>
2016-06-30 00:48:08 +0200

    Now using CMD script

--- Scripts/GoRepack.cmd
+++ Scripts/GoRepack.cmd
@@ -1,2 +1,1 @@
-powershell -File GoRepack.ps1
-pause
+..\packages\ILRepack.2.0.10\tools\ILRepack.exe /out:.\Output\DeveImagePyramid.exe ..\Devedse.DeveImagePyramid\bin\Release\Devedse.DeveImagePyramid.exe ..\Devedse.DeveImagePyramid\bin\Release\BitMiracle.LibTiff.NET.dll
